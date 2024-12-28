94567620bc42093b0b553c4b8e32fdae4c728be1
Avneesh Kumar <kaviikumar010@gmail.com>
2024-12-28 15:33:21 +0530

    logout backend completed

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -6,49 +6,55 @@ import createTokenAndSaveCookies from "../jwt/AuthToken.js"
 
 
 export const register=async(req,res)=>{
-if(!req.files||Object.keys(req.files).length===0){
-    return res.status(400).json({message:"User Photo is Required ! "})
-}
-const {photo}=req.files;
-const allowedFormats=["image/jpeg","image/png","image/webp"]
-if(!allowedFormats.includes(photo.mimetype))
+try{
+    if(!req.files||Object.keys(req.files).length===0){
+        return res.status(400).json({message:"User Photo is Required ! "})
+    }
+    const {photo}=req.files;
+    const allowedFormats=["image/jpeg","image/png","image/webp"]
+    if(!allowedFormats.includes(photo.mimetype))
+    {
+        return res.status(400).json({message:"Invalid photo formate. Only jpg and png are allowed"})
+    }
+        const {email,name,password,phone,education,role}=req.body;
+        if(!email|| !name || !password|| !phone||!education|| !role||!photo)
+        {
+            return res.status(400).json({message:"All fields are required"})
+        }
+        const user = await User.findOne({ email });
+        if(user)
+        {
+            return res.status(400).json({message:"User Already exists with this email"});
+        }
+    
+        const cloudinaryResponse=await cloudinary.uploader.upload(
+            photo.tempFilePath
+        )
+        if(!cloudinaryResponse || cloudinaryResponse.error){
+            console.log(cloudinaryResponse.error)
+        }
+    
+     const hashedPassword   =await bcrypt.hash(password,10);
+    const newUser=new User({
+        email,
+        name,
+         password:hashedPassword,
+         phone,
+          education, 
+          role, 
+          photo:{
+        public_id:cloudinaryResponse.public_id,
+        url:cloudinaryResponse.url,
+    }});
+    await newUser.save()
+    if(newUser){
+       const token= await createTokenAndSaveCookies(newUser._id,res)
+        res.status(201).json({message:"User registered Successfully",newUser,token:token})
+    }
+}catch(error)
 {
-    return res.status(400).json({message:"Invalid photo formate. Only jpg and png are allowed"})
-}
-    const {email,name,password,phone,education,role}=req.body;
-    if(!email|| !name || !password|| !phone||!education|| !role||!photo)
-    {
-        return res.status(400).json({message:"All fields are required"})
-    }
-    const user = await User.findOne({ email });
-    if(user)
-    {
-        return res.status(400).json({message:"User Already exists with this email"});
-    }
-
-    const cloudinaryResponse=await cloudinary.uploader.upload(
-        photo.tempFilePath
-    )
-    if(!cloudinaryResponse || cloudinaryResponse.error){
-        console.log(cloudinaryResponse.error)
-    }
-
- const hashedPassword   =await bcrypt.hash(password,10);
-const newUser=new User({
-    email,
-    name,
-     password:hashedPassword,
-     phone,
-      education, 
-      role, 
-      photo:{
-    public_id:cloudinaryResponse.public_id,
-    url:cloudinaryResponse.url,
-}});
-await newUser.save()
-if(newUser){
-   const token= await createTokenAndSaveCookies(newUser._id,res)
-    res.status(201).json({message:"User registered Successfully",newUser,token:token})
+console.log(error)
+return res.status(500).json({message:"Internal Server Error"})
 }
 };
 
@@ -84,16 +90,22 @@ token:token
 });
 
 
-
-
-
-
-
-
     }catch(error)
     {
 console.log(error)
 return res.status(500).json({error:"Internal Server error"})
     }
 
+}
+
+export const logout=async(req,res)=>{
+  try{
+     res.clearCookie("jwt",{httpOnly:true});
+  res.status(200).json({message:"User Logged out Successfully"}); 
+}
+  catch(error)
+  {
+console.log(error)
+return res.status(500).json({message:"Internal Server error"})
+  }
 }
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { login, register } from '../controller/user.controller.js';
+import { login, logout, register } from '../controller/user.controller.js';
 
 
 const router = express.Router(); // Use `router` consistently
 
 router.post('/register', register);
 router.post('/login',login)
+router.get('/logout',logout)
 
 export default router; // Export the router
